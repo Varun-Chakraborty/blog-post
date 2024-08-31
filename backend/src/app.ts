a7750a41079b29c4563cb3db778628ced6546ce1
Varun <varunchakraborty9526@gmail.com>
2024-08-31 18:47:23 +0530

    removed debug code and add dotenv

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,13 +3,15 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express();
 
 const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [];
 
 app.use(cors({
     origin: function (origin, callback) {
-      console.log(origin, allowedOrigins, origin ? allowedOrigins.indexOf(origin) : undefined, process.env);
       if (!origin || allowedOrigins.indexOf(origin) !== -1) callback(null, true);
       else callback(new Error('Not allowed by CORS'));
     },
